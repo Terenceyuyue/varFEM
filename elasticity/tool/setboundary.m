d8a88dd39f603a4c05667eac5dc071fa5ced27cc
terenceyuyue <terenceyuyue@sjtu.edu.cn>
2019-09-20 23:30:36 +0800

    delete old files

--- elasticity/tool/setboundary.m
+++
@@ -1,8 +0,0 @@
-function bdFlag = setboundary(elem)
-
-totalEdge = sort([elem(:,[2,3]); elem(:,[3,1]); elem(:,[1,2])],2);
-[i,j,s] = find(sparse(totalEdge(:,2),totalEdge(:,1),1));
-edge = [j,i];
-bdEdge = edge(s==1,:);
-
-bdFlag.eD = unique(bdEdge);
