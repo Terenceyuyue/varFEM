db5193b985ecf0df20c01968156fa35cbe192e66
terenceyuyue <terenceyuyue@sjtu.edu.cn>
2019-09-21 18:57:57 +0800

    Delete Contents.m

--- fem/Contents.m
+++
@@ -1,9 +0,0 @@
-% mVEM
-%
-% Files
-%   FEM1D                   - P1 FEM of 1-D partial diffential equation 
-%   elasticityVector        - Conforming P1 FEM of linear elasticity equation (programming in vectorized FE space)
-%   elasticityScalar        - Conforming P1 FEM of linear elasticity equation (programming in FE space of each component)
-%   elasticityLaplace       - Conforming P1 FEM of linear elasticity equation (bilinear form --- Laplacian)
-%   gradbasis               - gradient of barycentric basis
-
