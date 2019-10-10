d5f23958acd6b0cb2549678cef2f5295b6d8a798
terenceyuyue <terenceyuyue@sjtu.edu.cn>
2019-10-10 21:33:25 +0800

    delete others

--- fem/elasticity/Dquad.m
+++
@@ -1,8 +0,0 @@
-function D = Dquad(i,j,iel,Dphi,lambda)
-
-Deriv = @(s,t) Dphi(iel,s,i).*Dphi(iel,t,j) ...
-                + Dphi(iel,t,i).*Dphi(iel,s,j);
-
-D = [Deriv(1,1),Deriv(2,2),Deriv(1,2)];
-nI = size(lambda,1);
-D = repmat(D,nI,1);
