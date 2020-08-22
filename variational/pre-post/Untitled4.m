9706a448b80cb4b3fd1edf7eb7f50bb6c2dfc70f
terenceyuyue <terenceyuyue@sjtu.edu.cn>
2020-08-22 22:13:35 +0800

    Delete Untitled4.m

--- variational/pre-post/Untitled4.m
+++
@@ -1,8 +0,0 @@
-clc;clear;close all
-
-[node,elem] = squaremesh([0,1,0,1],0.5);
-showmesh(node,elem); 
-findnode(node); findelem(node,elem); findedge(node,elem);
-
-bdNeumann = 'abs(x-0)<1e-4';
-bdStruct = setboundary(node,elem,bdNeumann);
