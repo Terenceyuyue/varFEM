eb61bdeebe0993fb72274a33230e2ea1297be24f
Terenceyuyue <terenceyuyue@sjtu.edu.cn>
2022-06-09 22:22:37 +0800

    delete

--- varFEM/tool/varcontourf.m
+++
@@ -1,8 +0,0 @@
-function [c,h] = varcontourf(x,y,node,elem,uh,n)
-
-if nargin==5, n = 20; end
-
-[x,y] = meshgrid(x,y);
-uxy = pdeInterpolant2d(x,y,node,elem,uh);
-[c,h] = contourf(x,y,uxy,n); axis equal
-color = jet(10);  colormap(color);
--- varFEM/tool/varquiver.m
+++
@@ -1,7 +0,0 @@
-function varquiver(x,y,node,elem,uh,varargin)
-% uh = [u, v]
-
-[x,y] = meshgrid(x,y);
-u = varInterpolant2d(x,y,node,elem,uh(:,1));
-v = varInterpolant2d(x,y,node,elem,uh(:,2));
-quiver(x(:),y(:),u(:),v(:),varargin{:});
