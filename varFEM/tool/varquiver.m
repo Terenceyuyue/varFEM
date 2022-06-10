93e003a676aa8568f841b4884043694bbd67ee90
Terenceyuyue <terenceyuyue@sjtu.edu.cn>
2022-06-10 14:17:31 +0800

    delete

--- varFEM/tool/varquiver.m
+++
@@ -1,7 +0,0 @@
-function varquiver(x,y,node,elem,uh,varargin)
-% uh = [u, v]
-
-[x,y] = meshgrid(x,y);
-u = pdeInterpolant2d(x,y,node,elem,uh(:,1));
-v = pdeInterpolant2d(x,y,node,elem,uh(:,2));
-quiver(x(:),y(:),u(:),v(:),varargin{:});
